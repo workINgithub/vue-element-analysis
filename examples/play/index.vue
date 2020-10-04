96f1fc1069f8b3995ac9838db768064457f2bdce
workkk <gentlebob1998@icloud.com>
2020-10-04 16:10:28 +0800

    init element store

---
+++ examples/play/index.vue
@@ -0,0 +1,36 @@
+<template>
+  <div style="margin: 20px;">
+    <!-- <el-input v-model="input" placeholder="请输入内容"></el-input> -->
+    <!-- <el-tag>play ground</el-tag> -->
+    <input type="text">
+    <el-switch v-model="switchVal" ref="switch" @input="reportInactiveValue"></el-switch>
+  </div>
+</template>
+
+<script>
+  export default {
+    data() {
+      return {
+        input: 'Hello Element UI!',
+        switchVal: true
+      };
+    },
+
+    mounted () {
+      this.$refs.switch.focus()
+    },
+
+    methods: {
+      askAPI (value) {
+        console.log(value)
+        setTimeout(() => {
+          console.log('2s')
+          this.switchVal = ! this.switchVal
+        }, 2000)
+      },
+      reportInactiveValue (iv) {
+        console.log(iv)
+      }
+    }
+  };
+</script>
---
+++ zhf.md
@@ -0,0 +1,10 @@
+# 笔记
+
+```bash
+npm run dev:play
+```
+
+这个入口文件是/examples的play.js文件，然后渲染的组件是/examples/play/index.vue
+
+
+我觉得这样设计的原因就是给用户去玩的内容。
